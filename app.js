1e5a690f22d735c29f4699abcc7544a84db536a9
irfan <irfan.yeniceri@gmail.com>
2022-05-28 19:42:54 +0300

    Projedeki ilerlemeler güncellendi.

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
 
-app.get('/', function (req, res) {
 
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.send(blog);
+//Template Engine
 
-})
+app.set("view engine", "ejs");
+
+
+// Middlewares
+app.use(express.static('public'));
+
+app.get("/index.html", function (req, res) {
+  //const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  //res.send(blog);
+    res.render("index")
+});
+
+app.get("/about.html", function (req, res) {
+    //const blog = { id: 1, title: "Blog title", description: "Blog description" };
+    //res.send(blog);
+      res.render("about")
+  });
+
+  app.get("/add_post.html", function (req, res) {
+    //const blog = { id: 1, title: "Blog title", description: "Blog description" };
+    //res.send(blog);
+      res.render("about")
+  });
+
+
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı.`)
-})
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
